0d93429b65feb997d37d4eb85b903cf2e891feee
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-12-14 06:31:36 +0100

    💎💎�💎

--- controller/products.js
+++ controller/products.js
@@ -1,10 +1,22 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  // throw new Error('testing async errors');
-  res.status(200).json({msg:"Testing static products"});
+  const products = await Product.find({ page: "2" });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
-const getAllProducts = async(req, res) => {
-  res.status(200).json({ msg: `Products ready` });
+const getAllProducts = async (req, res) => {
+  const { featured, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
