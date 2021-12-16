b2768f6c369cf56ebe60487c807a4c8e7b293067
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-12-16 05:39:02 +0100

    ⚡🚀

--- controller/products.js
+++ controller/products.js
@@ -1,8 +1,9 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "x";
-  const products = await Product.find({}).select("name price").sort("price");
+  // const search = "x";
+  const products = await Product.find({ price: { $gt: 30 } }).sort("price");
+
   // const products = await Product.find({
   //   name: { $regex: search, $options: "i" },
   // });
@@ -10,7 +11,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -33,6 +34,36 @@ const getAllProducts = async (req, res) => {
     const fieldList = fields.split(",").join(" ");
     result = result.select(fieldList);
   }
+
+  if (numericFilters) {
+    const operatorMap = {
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+      ">=": "$gte",
+      ">": "$gt",
+    };
+
+    const regEx = /\b(<|>|>=|<=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [fields, operator, value] = item.split("-");
+      if (options.includes(fields)) {
+        queryObject[fields] = { [operator]: Number(value) };
+      }
+    });
+    console.log(queryObject );
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = Number(page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
